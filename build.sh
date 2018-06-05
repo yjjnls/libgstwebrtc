a31a5767ddb48491fcc6a6aa6a5c915699dc7150
yuanjunjie <yanjunjie@kedacom.com>
2018-06-05 10:44:01 +0800

    linux build

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
-mkdir -p build
+export GSTREAMER_ROOT=~/cerbero/build/dist/linux_x86_64/
+mkdir -p build/dtls
 
-pushd build
-cmake ..
+pushd build/dtls
+cmake ../../dtls
+make 
+make install
+
+popd
+mkdir -p build/webrtcbin
+
+pushd build/webrtcbin
+cmake ../../webrtc
 make 
 make install
 
